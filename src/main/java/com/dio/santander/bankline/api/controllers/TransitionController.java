b6048cbd0c4d255e31d404c32144c6760425289e
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-05-08 03:02:44 -0300

    :sparkles: feat: Add new feature

--- src/main/java/com/dio/santander/bankline/api/controllers/TransitionController.java
+++ src/main/java/com/dio/santander/bankline/api/controllers/TransitionController.java
@@ -33,4 +33,5 @@ public class TransitionController {
 	@GetMapping("/{idAccount}")
 	public List<Transition> findAll(@PathVariable("idAccount") Integer idAccount){
 		return repository.findByIdAccount(idAccount);
+	}
 }
